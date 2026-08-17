f69d3db3b455e078392bb454528b11b07c31a32b
Somnath <137623775+Somnathrosy@users.noreply.github.com>
2026-08-18 00:36:14 +0530

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/farm_website/">
       <ThemeProvider>
         <App />
       </ThemeProvider>
